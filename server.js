3558cbc1c5697f2ec7faea77ed95a564271c468a
Julien WALLE <jwalle@e2r8p8.42.fr>
2017-08-30 18:45:36 +0200

    setup

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 'use strict';
 
-const express = require('express');
+const app = require('express')()
+let fs = require('fs');
 
+app.set('view engine', 'ejs')
 // Constants
-const PORT = 8060;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
+// app.get('/', (req, res) => {
+// 	fs.readFile('index.html', (err, data) => {
+// 		if (err) throw err
+// 		res.writeHead(200, {
+// 			'content-type': 'text/html; charset=utf-8'
+// 		})
+// 		res.end(data)
+// 	})
+// });
+
 app.get('/', (req, res) => {
-	res.send('Hello world\n');
-});
+	res.render('pages/index', {test:'jwalle'})
+})
+
+
 
 app.listen(PORT,HOST);
-console.log("Running on http://${HOST}:${PORT}");
+console.log(`Running on http://${HOST}:${PORT}`);
